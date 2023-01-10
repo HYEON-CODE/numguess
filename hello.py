e40670ffa49b648d6d102994e20068c84f79719b
HYEON-CODE <gus7264@gmail.com>
2023-01-10 09:35:44 +0900

    feat: Add container(trailing comma test)
    Before

--- hello.py
+++ hello.py
@@ -2,3 +2,12 @@
 username = "Gwan Hyeon"
 # Print greeting with username
 print('Hello, ', username)
+
+# Don't do
+animals = ['cat', 'dog']
+
+# Do(Trailing comma)
+menus = [
+    'Tteokbokki',
+    'soup',
+]
