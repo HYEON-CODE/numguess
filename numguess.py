944a6cc3e7494add705e38f493a144ade9b0878c
HYEON-CODE <gus7264@gmail.com>
2023-01-10 15:08:55 +0900

    feat: Add comparig user's guess with answer
    만약, 유저가 입력한 값과 정답과 일치하면 정답을 출력
    그렇지 않다면,
    입력한 값이 정답보다 작다면 힌트 출력
    입력한 값이 정답보다 크다면 힌트 출력

--- numguess.py
+++ numguess.py
@@ -1,4 +1,5 @@
 from random import randint
+from time import sleep
 
 # answer range 1~100
 answer = randint(1,100)
@@ -11,3 +12,17 @@ username = input("Hi there, What is your name?")
 print(f"Hi,{username}! Please be my guest!!")
 guess = int(input(f"So {username}, Guess the number(1-100): "))
 print(f'Well choice {username}~~ You picked {guess}!!')
+
+# Compare answer with user's guess
+if answer==guess:
+    print("************************")
+    sleep(1)
+    print("************************")
+    sleep(1)
+    print("************************")
+    sleep(1)        
+    print(f"Wow! Your got is right!! The answer is {answer}")
+elif guess>answer:
+    print(f"Keep going, That was too high")
+elif guess<answer:
+    print(f"Keep going, That was too low")
