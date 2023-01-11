08e18cf85b0b49288f1889d952a12537fb8d860d
HYEON-CODE <gus7264@gmail.com>
2023-01-11 16:36:57 +0900

    feat: Add bonus score and print score

--- numguess.py
+++ numguess.py
@@ -4,7 +4,8 @@ from time import sleep
 def game_start():
     # answer range 1~100
     answer = randint(1,100)
-    game_count = 10 # 게임 반복 횟수
+    trial = 10 # 게임 반복 횟수
+    base_score=[100]
 
     # Print answer
     print(answer)
@@ -14,15 +15,16 @@ def game_start():
     print(f"Hi,{username}! Please be my guest!!")
 
     # Compare answer with user's guess
-    while game_count>1:
+    while trial>1:
+        trial-=1
         guess = int(input(f"So {username}, Guess the number(1-100): "))
-        game_count-=1
         if answer==guess:
-            print(f"Wow! Your got is right!! The answer is {answer}\n")
+            print(f"Wow! Your got is right!! The answer is {answer} bonus score+{trial+1}")
             break
         elif guess>answer:
-            print(f"Keep going, That was too high. \nchance left:{game_count}")
+            print(f"Keep going, That was too high. \nchance left:{trial}")
         elif guess<answer:
-            print(f"Keep going, That was too low \nchance left:{game_count}")
-
+            print(f"Keep going, That was too low \nchance left:{trial}")
+    new_score=[i+trial+1 for i in base_score]
+    print(f"your score is {new_score}")
 game_start()
