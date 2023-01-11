89bd2b43dcec705f7a6219e483aa9ec1e60a67f9
HYEON-CODE <gus7264@gmail.com>
2023-01-11 16:09:39 +0900

    feat: Add function and Create gamecount

--- numguess.py
+++ numguess.py
@@ -1,28 +1,33 @@
 from random import randint
 from time import sleep
 
-# answer range 1~100
-answer = randint(1,100)
 
-# Print answer
-print(answer)
+def game_start():
+    # answer range 1~100
+    answer = randint(1,100)
+    game_count = 10 # 게임 반복 횟수
 
-# User interaction
-username = input("Hi there, What is your name?")
-print(f"Hi,{username}! Please be my guest!!")
-guess = int(input(f"So {username}, Guess the number(1-100): "))
-print(f'Well choice {username}~~ You picked {guess}!!')
+    # Print answer
+    print(answer)
 
-# Compare answer with user's guess
-if answer==guess:
-    print("************************")
-    sleep(1)
-    print("************************")
-    sleep(1)
-    print("************************")
-    sleep(1)        
-    print(f"Wow! Your got is right!! The answer is {answer}")
-elif guess>answer:
-    print(f"Keep going, That was too high")
-elif guess<answer:
-    print(f"Keep going, That was too low")
+    # User interaction
+    username = input("Hi there, What is your name?")
+    print(f"Hi,{username}! Please be my guest!!")
+    guess = int(input(f"So {username}, Guess the number(1-100): "))
+    print(f'Well choice {username}~~ You picked {guess}!!')
+
+    # Compare answer with user's guess
+    if answer==guess:
+        print("************************")
+        sleep(1)
+        print("************************")
+        sleep(1)
+        print("************************")
+        sleep(1)        
+        print(f"Wow! Your got is right!! The answer is {answer}")
+    elif guess>answer:
+        print(f"Keep going, That was too high")
+    elif guess<answer:
+        print(f"Keep going, That was too low")
+
+game_start()
